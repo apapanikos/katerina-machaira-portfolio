ee4888ab30624e9f2929261a63958c88e76a11ed
Alex Papanikos <apapanikos@convertgroup.com>
2021-10-26 14:21:09 +0300

    fix hero elements dimensions

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.leftContent}>
-          <h1>UI / UX</h1>
+          <h1>UI/UX</h1>
           <h1>Designer</h1>
         </div>
       </div>
@@ -15,6 +15,10 @@ const Hero = () => {
             <h1>Port</h1>
             <h1>Folio.</h1>
           </div>
+        </div>
+        <div className={styles.subtitle}>
+          <h3>Hello I am Katerina Machaira</h3>
+          <h3>I am a Ui/Ux Designer</h3>
         </div>
       </div>
     </div>
