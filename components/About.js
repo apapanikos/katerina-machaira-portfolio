3ab4ac0b096a9c32a489fa5a59fd14218dcb2724
Alex Papanikos <apapanikos@convertgroup.com>
2021-10-30 19:16:39 +0300

    style about section

--- components/About.js
+++ components/About.js
@@ -11,24 +11,36 @@ const socialIcons = [
   {source: linkedinIcon, alt: 'linkedin'},
 ]
 
+// TODO import pdf file
+const resume = ''
+
 const about = 'Hello my name is Katerina Machaira and I am a passionate entry level Ui/Ux designer based in Greece. Hello my name is Katerina Machaira and I am a passionate. Hello my name is Katerina Machaira and I am a passionate entry level Ui/Ux designer based in Greece.'
 
 const About = () => {
-  console.log(socialIcons[0].source)
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <p>{about}</p>
-        <div></div>
-        <ul>
+        <a href={resume} without rel="noopener noreferrer" target="_blank" className={styles.resumeBtn}>View my resume</a>
+        <ul className={styles.icons}>
           {socialIcons.map((icon, index) => {
             return <li key={index}>
-              <Image src={icon.source} alt={icon.alt} layout="fill"/>
+              <Image src={icon.source} alt={icon.alt} width='30px' height='30px'/>
             </li>
           })}
         </ul>
       </div>
-      <div className={styles.right}></div>
+      <div className={styles.right}>
+        <div className={styles.tile}>
+          <div className={styles.title}>
+            <h1>About me.</h1>
+            <div className={styles.divider}></div>
+          </div>
+          <h3 className={styles.subtitle}>
+            Get to know me.
+           </h3>
+        </div>
+      </div>
     </div>
   )
 }
