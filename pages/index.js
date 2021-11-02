63883ef727c01d9794f3f113f2bfec6abfdd48d4
Alex Papanikos <apapanikos@convertgroup.com>
2021-11-02 20:43:57 +0200

    create fetch api helper function to retrieve projects

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,18 @@ import Sidebar from '../components/Sidebar'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import { fetchAPI } from '../lib/api'
 
-export default function Index() {
+export default function Index({ projects }) {
   return (
     <>
     <Hero />
     <About />
+    {projects && projects.map((project) => (
+      <div key={project.id}>
+        <h1>{project.title}</h1>
+      </div>
+    ))}
     </>
   )
 }
@@ -22,4 +28,13 @@ Index.getLayout = function getLayout(page) {
       {page}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  // get projects from api using the helper function
+  const projects = fetchAPI('/projects');
+
+  return {
+    props: { projects },
+  }
 }
