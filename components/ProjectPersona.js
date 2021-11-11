6c004e6dd20b1c74e4d39ad47a6765855edb4ca0
Alex Papanikos <apapanikos@convertgroup.com>
2021-11-11 23:48:10 +0200

    finish persona section styles

--- components/ProjectPersona.js
+++ components/ProjectPersona.js
@@ -13,6 +13,28 @@ const ProjectPersona = ({ project }) => {
           {/* <Image src={getStrapiMedia(persona?.avatar)} alt={persona.avatar.name} width={persona.avatar.width} height={persona.avatar.height}/> */}
           <p>"{persona.desc}"</p>
         </div>
+        <div className = 'row mt-12'>
+          <div className = 'col'>
+            <h5 className={styles['project-persona__content__name']}>{persona.name}</h5>
+            <div className={styles['project-persona__content__age']}>
+              <span className='font-extrabold'>Age: </span> {persona.age}
+            </div>
+            <div className={styles['project-persona__content__edu']}>
+              <span className='font-extrabold'> Education: </span> {persona.education}
+            </div>
+            <div className={styles['project-persona__content__family']}>
+              <span className='font-extrabold'> Family: </span> {persona.family}
+            </div>
+          </div>
+          <div className="col">
+            <h5 className={styles['project-persona__content__goals']}>Goals</h5>
+            <p dangerouslySetInnerHTML={{ __html: persona.goals}}></p>
+          </div>
+          <div className="col">
+            <h5 className={styles['project-persona__content__frustrations']}>Frustrations</h5>
+            <p dangerouslySetInnerHTML={{ __html: persona.frustrations}}></p>
+          </div>
+        </div>
       </div>
     </div>
   )
