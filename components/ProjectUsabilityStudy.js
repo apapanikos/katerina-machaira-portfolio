aff1ff8cd07273f2607373b4bd9478ca4c94ccfc
Alex Papanikos <apapanikos@convertgroup.com>
2021-11-15 19:51:10 +0200

    check for usability study findings length in orderr to render content properly

--- components/ProjectUsabilityStudy.js
+++ components/ProjectUsabilityStudy.js
@@ -3,13 +3,14 @@ import styles from '../styles/ProjectUsabilityStudy.module.css'
 
 const ProjectUsabilityStudy = ({ project }) => {
   const usabilityStudy = project?.usability_study
+  const firstRoundTitle = usabilityStudy?.second_round_findings?.length > 0 ? 'ROUND 1 FINDINGS' : 'Findings'
   return (
     <div className={styles['project-usability-study']}>
       <div className={styles['project-usability-study__content']}>
         <h1>Usability Stduy.</h1>
         <p>{usabilityStudy.desc}</p>
         <div className={styles['project-usability-study__content__findings']}>
-          <h3>ROUND 1 FINDINGS</h3>
+          <h3>{firstRoundTitle}</h3>
           <ul className={styles['project-usability-study__content__findings__list']}>
             {usabilityStudy?.first_round_findings && usabilityStudy?.first_round_findings.map((finding, index) => 
               <li key={index}>
@@ -19,17 +20,19 @@ const ProjectUsabilityStudy = ({ project }) => {
             )}
           </ul>
         </div>
-        <div className={styles['project-usability-study__content__findings']}>
-          <h3>ROUND 2 FINDINGS</h3>
-          <ul className={styles['project-usability-study__content__findings__list']}>
-            {usabilityStudy?.second_round_findings && usabilityStudy?.second_round_findings.map((finding, index) => 
-              <li key={index}>
-                <span>{index + 1}.</span>
-                <div className={styles['project-usability-study__content__findings__list__value']}>{finding?.value}</div>
-              </li>
-            )}
-          </ul>
-        </div>
+        {usabilityStudy?.second_round_findings?.length > 0 && (
+          <div className={styles['project-usability-study__content__findings']}>
+            <h3>ROUND 2 FINDINGS</h3>
+            <ul className={styles['project-usability-study__content__findings__list']}>
+              {usabilityStudy?.second_round_findings && usabilityStudy?.second_round_findings.map((finding, index) => 
+                <li key={index}>
+                  <span>{index + 1}.</span>
+                  <div className={styles['project-usability-study__content__findings__list__value']}>{finding?.value}</div>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   )
