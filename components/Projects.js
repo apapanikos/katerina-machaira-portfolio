38f8bba4c0714bd148504e78881829cba840969d
Alex Papanikos <apapanikos@convertgroup.com>
2021-11-20 15:57:45 +0200

    remove mock data

--- components/Projects.js
+++ components/Projects.js
@@ -3,11 +3,6 @@ import styles from '../styles/Projects.module.css'
 import ProjectCard from './ProjectCard'
 
 const Projects = ({ projects }) => {
-  const MOCK_PROJECTS = [
-    {id: 1, title: 'Eaters', description: 'Mobile food app.'},
-    {id: 2, title: 'Estate', description: 'Real estate responsive website.'},
-    {id: 3, title: 'Medio', description: 'Medicine reminder cross - platform tool.'},
-  ]
   return (
     <section id="projects" className={styles.container}>
       <h1>Projects</h1>
